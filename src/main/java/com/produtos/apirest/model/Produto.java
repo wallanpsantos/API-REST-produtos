9c2192dd065cb1ab88d69fae1909f9a0564128b3
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-03-08 20:17:00 -0300

    API REST com Spring Boot - parte 3: Implementando os métodos GET e POST
    Continuar aula API REST com Spring Boot - parte 3: Implementando os métodos GET e POST: https://www.youtube.com/watch?v=e0ItyfvbhMw&list=PL8iIphQOyG-D2FP9wkg12AavzmVRWEcnJ&index=3

---
+++ src/main/java/com/produtos/apirest/model/Produto.java
@@ -0,0 +1,75 @@
+package com.produtos.apirest.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private BigDecimal quantidade;
+
+	private BigDecimal valor;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the quantidade
+	 */
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	/**
+	 * @param quantidade the quantidade to set
+	 */
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
