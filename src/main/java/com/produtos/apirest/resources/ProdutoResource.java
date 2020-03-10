af043b04caebf11548fdc40d7b538d22015b78e7
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-03-10 10:09:25 -0300

    API REST com Spring Boot - parte 3: Implementando os métodos GET e POST
    Criando o POST  - 12'52''

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.produtos.apirest.repository.ProdutoRepository;
@@ -22,11 +25,14 @@ public class ProdutoResource {
 	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
-	
+
 	@GetMapping("/produtos/{id}")
-	public Produto produtoUnico(@PathVariable(value="id") long id) {
+	public Produto produtoUnico(@PathVariable(value = "id") long id) {
 		return produtoRepository.findById(id);
 	}
-	
-	
+
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 }
