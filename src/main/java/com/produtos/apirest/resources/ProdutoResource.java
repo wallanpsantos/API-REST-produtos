cc0a71345c994a0dbc8bbd51c982407189023e03
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-03-11 02:05:23 -0300

    API REST com Spring Boot - parte 5: Swagger
    Iniciar aula: API REST com Spring Boot - parte 5: Swagger

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,13 +3,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.produtos.apirest.repository.ProdutoRepository;
 import com.produtos.apirest.model.Produto;
@@ -33,6 +27,16 @@ public class ProdutoResource {
 
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@DeleteMapping("/produto")
+	public void deleteProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+
+	@PutMapping("/produto")
+	public Produto atualizarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 }
